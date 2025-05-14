bef8eb6e9027b98e95d11cb4e951599db257d374
jrnasciment <jrnna@jrnasciment>
2025-05-13 21:38:58 -0300

    Time function with invoke (show)

--- cypress/e2e/privacyPolicy.cy.js
+++ cypress/e2e/privacyPolicy.cy.js
@@ -2,8 +2,8 @@ Cypress._.times(10, () => {
   it.only('testa a página da política de privacidade de forma independente', () => {
     cy.visit('./src/privacy.html')
 
-    cy.contains('h1', 'CAC TAT - Política de Privacidade').invoke('hide')
-      .should('not. be.visible')
+    cy.contains('h1', 'CAC TAT - Política de Privacidade').invoke('show')
+      .should('be.visible')
     cy.contains('p', 'Talking About Testing')
       .should('be.visible')
   })
